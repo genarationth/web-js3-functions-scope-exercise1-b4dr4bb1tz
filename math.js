454d3d0dab898f631fd756a8a96e2ecc7736fd12
Soravit Sakukumpat <dareal.u.ess@gmail.com>
2024-05-14 14:01:24 +0700

    ex1

---
+++ math.js
@@ -0,0 +1,16 @@
+function mathCal(x, y){
+    console.log(x+y);
+    console.log(x-y);
+    console.log(x*y);
+    console.log(x/y);
+    console.log(x%y);
+    console.log(x**y);
+}
+mathCal(1,2);
+
+
+function inForm(firstName, location, hobby) {
+    console.log(`Hi, my name is ${firstName} I live in ${location} and enjoy to ${hobby}`);
+}
+
+inForm('Soravit', 'Bangkok', 'Playgames');
